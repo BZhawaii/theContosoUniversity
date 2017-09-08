ff063e5a68fd14bbb97c3889a969fd84c9096f44
bzhawaii <bzhawaii@yahoo.com>
2017-09-08 12:09:28 -0600

    Added seed data

---
+++ theContosoUniversity/Migrations/201709081640327_fixCourseModel.cs
@@ -0,0 +1,16 @@
+namespace theContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fixCourseModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
