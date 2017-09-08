814dce2cd40748e77664108057946ce00e26ac23
bzhawaii <bzhawaii@yahoo.com>
2017-09-08 16:46:39 -0600

    Added DepartmentSP migrations and have access to create new department.

--- theContosoUniversity/DAL/SchoolContext.cs
+++ theContosoUniversity/DAL/SchoolContext.cs
@@ -18,6 +18,12 @@ namespace theContosoUniversity.DAL
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+                .Map(t => t.MapLeftKey("CourseID")
+                    .MapRightKey("InstructorID")
+                    .ToTable("CourseInstructor"));
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
         }
 
         public System.Data.Entity.DbSet<theContosoUniversity.Models.Department> Departments { get; set; }
